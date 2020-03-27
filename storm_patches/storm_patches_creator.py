ab3ae4670a00bd751c5b836f8e47e59223227430
Maria Molina <hurricanemolina@gmail.com>
2020-03-26 22:33:05 -0600

    add variable storm_patch functions to creator

--- storm_patches/storm_patches_creator.py
+++ storm_patches/storm_patches_creator.py
@@ -31,7 +31,8 @@ class storm_patch_creator:
     def __init__(self, date1, date2, climate, destination_path, 
                  min_dbz=20, max_dbz=40, patch_radius=16, method='ws',
                  dbz_path='/gpfs/fs1/collections/rda/data/ds612.0/',
-                 uh25_path=None, uh03_path=None, ctt_path=None,
+                 uh25_path=None, uh03_path=None, ctt_path=None, 
+                 variable=None, variable_path=None,
                  num_cpus=36):
         
         """
@@ -53,6 +54,8 @@ class storm_patch_creator:
         uh25_path: directory where the updraft helicity (2-5-km) data is contained (str, default None)
         uh03_path: directory where the updraft helicity (0-3-km) data is contained (str, default None)
         ctt_path: directory where the cloud top temperature data is contained (str, default None)
+        variable: name of variable (folder location) to be extracted for corresponding storm patches (str, default None)
+        variable_path: path to where variable files are located (str, default None)
         num_cpus: number of CPUs for to use in a node for parallelizing extractions (int; default 36)
         
         """
@@ -85,7 +88,35 @@ class storm_patch_creator:
         self.uh25_path = uh25_path
         self.uh03_path = uh03_path
         self.ctt_path = ctt_path
+                   
+        self.variable = variable
+        self.variable_path = variable_path
+        
         self.num_cpus = num_cpus
+        
+        
+        
+    def variable_translate(self):
+        
+        """
+            Variable name for the respective filenames.
+        """
+        
+        var = {
+               'EU':'EU',
+               'EV':'EV',
+               'TK':'TK',
+               'QVAPOR':'QVAPOR',
+               'WMAX':'MAXW',
+               'W_vert':'W',
+               'PRESS':'P'
+              }
+        
+        try:
+            out = var[self.variable]
+            return out
+        except:
+            raise ValueError("Please enter TK, EU, EV, QVAPOR, P, W_vert, or WMAX as variable.")
         
 
         
@@ -288,7 +319,7 @@ class storm_patch_creator:
         
         ###########################################################################
         
-        print(f"doing current: {datetime_value.strftime('%Y%m%d')}")
+        print(f"doing {self.climate}: {datetime_value.strftime('%Y%m%d')}")
 
         #open the original hourly storm patches that were created and saved
         file_storm = f"/{self.destination_path}/{self.climate}_SPhourly_{datetime_value.strftime('%Y%m%d')}.nc"
@@ -393,5 +424,152 @@ class storm_patch_creator:
         
         ###########################################################################
 
+        
+        
+    def create_patches_variable(self, datetime_value):
+
+        """
+            Function to extract variable data that corresponds to previously extracted 3-hour storm patches (with create_patches_3H).
+            Input: one day (datetime_value).
+        """
+
+        land = self.prep_land()
+        
+        ###########################################################################
+        
+        #default values for the WRF CONUS1 dataset below, 
+        #including the full time range for indexing simulations and slicing the CONUS spatially for efficiency 
+        #(we don't care about storms over water
+        total_times = pd.date_range('2000-10-01','2013-10-01 00:00:00',freq='H')
+        total_times_indexes = np.arange(0,total_times.shape[0],1)
+        the1=135; the2=650; the3=500; the4=1200
+        
+        #creation of blank lists for variable saving
+        var_to_return = []; DZ_to_return = []; MASK_to_return = []
+        ROW_to_return = []; COL_to_return = []; LAT_to_return = []; LON_to_return = []
+        STM_to_return = []; ETM_to_return = []; XSPD_to_return = []; YSPD_to_return = []
+        
+        ###########################################################################
+        
+        print(f"doing {self.climate}, {self.variable}: {datetime_value.strftime('%Y%m%d')}")
+        
+        file_storm = f"/{self.destination_path}/{self.climate}_SPhourly_{datetime_value.strftime('%Y%m%d')}.nc"
+        data_storm = xr.open_dataset(file_storm)
+
+        #opening the variable data set
+        if self.variable != 'WMAX':
+            file_var = f"/{self.variable_path}/{self.variable}/wrf2d_interp_{self.variable_translate()}_{datetime_value.strftime('%Y%m')}.nc"
+        if self.variable == 'WMAX':
+            file_var = f"/{self.variable_path}/{self.variable}/wrf2d_max_{self.variable_translate()}_{datetime_value.strftime('%Y%m')}.nc"
+        
+        try:
+            data_var = xr.open_dataset(file_var)
+        except IOError:
+            #very few days do not contain identified storms, avoid job kill with exception here.
+            print(f"not found {datetime_value.strftime('%Y%m%d')}")
+            return
+        
+        check_3H_datastorm = np.isin(data_storm.starttime.values,total_times_indexes[::3])
+
+        var_time_equiv_storm_patch = total_times_indexes[np.isin(total_times, 
+                  pd.date_range(datetime_value.strftime('%Y-%m')+'-01',
+                  pd.to_datetime(datetime_value.strftime('%Y-%m')+'-'+str(calendar.monthrange(datetime_value.year, datetime_value.month)[1]))+timedelta(hours=23),
+                  freq='3H'))]
+
+        for storm_patch_file_idx, (storm_time, storm_time_indx) in enumerate(zip(check_3H_datastorm, data_storm.starttime.values)):
+
+            if storm_time:
+
+                if self.is_land(land,
+                                data_storm.lons[storm_patch_file_idx,
+                                                np.where(data_storm.grid[storm_patch_file_idx,:,:]==data_storm.grid[storm_patch_file_idx,:,:].max())[0][0],
+                                                np.where(data_storm.grid[storm_patch_file_idx,:,:]==data_storm.grid[storm_patch_file_idx,:,:].max())[1][0]], 
+                                data_storm.lats[storm_patch_file_idx,
+                                                np.where(data_storm.grid[storm_patch_file_idx,:,:]==data_storm.grid[storm_patch_file_idx,:,:].max())[0][0],
+                                                np.where(data_storm.grid[storm_patch_file_idx,:,:]==data_storm.grid[storm_patch_file_idx,:,:].max())[1][0]]):
+
+                    if self.variable != 'WMAX':
+                        temp_var = data_var.levels[np.where(var_time_equiv_storm_patch==storm_time_indx)[0],:,the1:the2,the3:the4].values[0,:,:,:]
+                        var_to_return.append(temp_var[:, data_storm.row_indices[storm_patch_file_idx].values, data_storm.col_indices[storm_patch_file_idx].values])
+                    if self.variable == 'WMAX':
+                        temp_var = data_var.max_in_vert[np.where(var_time_equiv_storm_patch==storm_time_indx)[0],the1:the2,the3:the4].values[0,:,:]
+                        var_to_return.append(temp_var[data_storm.row_indices[storm_patch_file_idx].values, data_storm.col_indices[storm_patch_file_idx].values])
+                    
+                    DZ_to_return.append(data_storm.grid[storm_patch_file_idx,:,:].values)
+                    MASK_to_return.append(data_storm.mask[storm_patch_file_idx,:,:].values)
+                    ROW_to_return.append(data_storm.row_indices[storm_patch_file_idx,:,:].values)
+                    COL_to_return.append(data_storm.col_indices[storm_patch_file_idx,:,:].values)
+                    LAT_to_return.append(data_storm.lats[storm_patch_file_idx,:,:].values)
+                    LON_to_return.append(data_storm.lons[storm_patch_file_idx,:,:].values)
+                    STM_to_return.append(data_storm.coords['starttime'][storm_patch_file_idx].values)
+                    ETM_to_return.append(data_storm.coords['endtime'][storm_patch_file_idx].values)
+                    XSPD_to_return.append(data_storm.coords['x_speed'][storm_patch_file_idx].values)
+                    YSPD_to_return.append(data_storm.coords['y_speed'][storm_patch_file_idx].values)
 
 
+        if self.variable != 'WMAX':
+            data_assemble = xr.Dataset({
+                 'var_grid':(['starttime','levels','y','x'],np.array([obj for obj in var_to_return])),
+                 'dbz_grid':(['starttime','y','x'],np.array([obj for obj in DZ_to_return])),
+                 'mask':(['starttime','y','x'],np.array([obj for obj in MASK_to_return])),
+                 'row_indices':(['starttime','y','x'],np.array([obj for obj in ROW_to_return])),
+                 'col_indices':(['starttime','y','x'],np.array([obj for obj in COL_to_return])),
+                 'lats':(['starttime','y','x'],np.array([obj for obj in LAT_to_return])),
+                 'lons':(['starttime','y','x'],np.array([obj for obj in LON_to_return]))
+                },
+                 coords=
+                {'starttime':(['starttime'],np.array([float(obj) for obj in STM_to_return])),
+                 'endtime':(['starttime'],np.array([float(obj) for obj in ETM_to_return])),
+                 'x_speed':(['starttime'],np.array([int(obj) for obj in XSPD_to_return])),
+                 'y_speed':(['starttime'],np.array([int(obj) for obj in YSPD_to_return])),
+                 'levels':(['levels'],np.array([0,1,2,3]))
+                })
+            
+        if self.variable == 'WMAX':
+            data_assemble = xr.Dataset({
+                 'var_grid':(['starttime','y','x'],np.array([obj for obj in var_to_return])),
+                 'dbz_grid':(['starttime','y','x'],np.array([obj for obj in DZ_to_return])),
+                 'mask':(['starttime','y','x'],np.array([obj for obj in MASK_to_return])),
+                 'row_indices':(['starttime','y','x'],np.array([obj for obj in ROW_to_return])),
+                 'col_indices':(['starttime','y','x'],np.array([obj for obj in COL_to_return])),
+                 'lats':(['starttime','y','x'],np.array([obj for obj in LAT_to_return])),
+                 'lons':(['starttime','y','x'],np.array([obj for obj in LON_to_return]))
+                },
+                 coords=
+                {'starttime':(['starttime'],np.array([float(obj) for obj in STM_to_return])),
+                 'endtime':(['starttime'],np.array([float(obj) for obj in ETM_to_return])),
+                 'x_speed':(['starttime'],np.array([int(obj) for obj in XSPD_to_return])),
+                 'y_speed':(['starttime'],np.array([int(obj) for obj in YSPD_to_return])),
+                })            
+
+        data_assemble.to_netcdf(f"/{self.destination_path}/{self.climate}_SP3hourly_{self.variable_translate().lower()}_{datetime_value.strftime('%Y%m%d')}.nc")
+        print(f"completed {datetime_value.strftime('%Y%m%d')}")
+        return
+
+    
+    
+    def parallelizing_3Hvariable_func(self):
+            
+        """
+            Here, we activate the multiprocessing function again, and parallelize the creation of 3-hour variable extractions.
+        """
+
+        if not self.variable_path:
+            raise Exception(f"Please enter the directory path for {self.variable}.")
+        
+        times_thisfile = self.generate_timestring()
+
+        pool = mp.Pool(self.num_cpus)
+
+        for time_for_file in times_thisfile:
+
+            print(f"start {time_for_file.strftime('%Y%m%d')}")
+            pool.apply_async(self.create_patches_variable, args=([time_for_file]))
+
+        pool.close()
+        pool.join()  # block at this line until all processes are done
+        print("completed")
+        
+        ###########################################################################
+        
+        
