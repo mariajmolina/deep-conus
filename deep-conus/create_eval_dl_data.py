1c749abeed2628d0545bf9a33455c8f4192b5b5f
Maria Molina <hurricanemolina@gmail.com>
2020-04-06 21:45:49 -0600

    add module to create dl test data subsets

---
+++ deep-conus/create_eval_dl_data.py
@@ -0,0 +1,208 @@
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+
+class CreateEvaluationData:
+    
+    """Class instantiation of CreateEvaluationData:
+    
+    Here we create subsets of test data for evaluating the deep learning model.
+    
+    Attributes:
+        climate (str): Whether to interpolate variable in the ``current`` or ``future`` climate simulation.
+        method (str): Method for parsing data. Options include ``random``, ``month``, ``season``, ``year``.
+        variables (str): Numpy array of variable name strings. Options include ``EU``, ``EV``, ``TK``, ``QVAPOR``, ``WMAX``, 
+                         ``W_vert``,``PRESS``,``DBZ``,``CTT``,``UH25``, and``UH03``.
+        directory (str): Directory where the deep learning files are saved and where these test data subsets will be saved.
+        mask (boolean): Whether to train using the masked data or the non-masked data. Defaults to False.
+        season_choice (str): Three-month season string, if ``method==season`` (e.g., 'DJF'). Defaults to ``None``.
+        
+    Raises:
+        Exceptions: Checks whether correct values were input for ``climate``, ``method``, and ``project_code``.
+        
+    Todo:
+        * Add method for ``month``, ``season``, and ``year``.
+    
+    """
+    
+    def __init__(self, climate, method, variables, directory, mask=False, season_choice=None):
+        
+        if climate!='current' and climate!='future':
+            raise Exception("Please enter current or future as string for climate period selection.")
+        if climate=='current' or climate=='future':
+            self.climate=climate
+            
+        if method!='random' and method!='month' and method!='season' and method!='year':
+            raise Exception("Please enter ``random``, ``month``, ``season``, or ``year`` as method.")
+        if method=='random' or method=='month' or method=='season' or method=='year':
+            self.method=method
+            
+        self.variables=variables
+        self.directory=directory
+        
+        self.mask=mask
+        if not self.mask:
+            self.mask_str='nomask'
+        if self.mask:
+            self.mask_str='mask'
+        
+        
+    def variable_translate(self, variable):
+        
+        """Variable name for the respective filenames.
+           
+        Args:
+            variable (str): The variable to feed into the dictionary.
+            
+        Returns:
+            variable (str): The variable name to use for opening saved files.
+            
+        Raises:
+            ValueError: If provided variable is not available.
+            
+        """
+        var={
+               'EU':'EU',
+               'EV':'EV',
+               'TK':'TK',
+               'QVAPOR':'QVAPOR',
+               'WMAX':'MAXW',
+               'W_vert':'W',
+               'PRESS':'P',
+               'DBZ':'DBZ',
+               'CTT':'CTT',
+               'UH25':'UH25',
+               'UH03':'UH03',
+              }
+        try:
+            out=var[variable]
+            return out
+        except:
+            raise ValueError("Please enter ``TK``, ``EV``, ``EU``, ``QVAPOR``, ``PRESS``, ``W_vert``, ``UH25``, ``UH03``, ``MAXW``, ``CTT``, or ``DBZ`` as variable.")
+
+            
+    def open_files(self):
+
+        """Open the testing data files and apply the respective parsing method to create the test subset.
+            
+        """
+        all_data={}
+        
+        for var in self.variables:
+            print(f"Opening {var}...")
+            data=xr.open_mfdataset(f'/{self.directory}/{self.climate}_{self.variable_translate(var).lower()}_{self.mask_str}_dldata_traintest.nc',
+                                   parallel=True, combine='by_coords')
+            
+            if self.method=='random':
+                self.random_method(data, var)
+                
+            if self.method=='month':
+                self.month_method(data, var)
+                
+            if self.method=='season':
+                self.season_method(data, var)
+                
+            if self.method=='year':
+                self.year_method(data, var)
+                
+        return
+    
+    
+    def random_method(self, data, variable):
+        
+        """Randomizes and parses the test data into 6 groups, saving each individually to avoid memory issues during evaluation.
+        
+        Args:
+            data (Xarray data array): Test data for respective variable.
+            variable (str): The variable being processed and saved.
+        
+        """
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[:100000]
+        print(f"Opening 1...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 1...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test1.nc')
+        
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[100000:200000]
+        print(f"Opening 2...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 2...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test2.nc')
+        
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[200000:300000]
+        print(f"Opening 3...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 3...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test3.nc')
+        
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[300000:400000]
+        print(f"Opening 4...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 4...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test4.nc')
+        
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[400000:500000]
+        print(f"Opening 5...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 5...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test5.nc')
+        
+        np.random.seed(0)
+        select_data=np.random.permutation(data.coords['b'].shape[0])[500000:]
+        print(f"Opening 6...")
+        data_assemble = xr.Dataset({'X_test':(['b','x','y','features'], data.X_test.transpose('b','x','y','features')[select_data,:,:,:]),
+                                    'X_test_label':(['b'], data.X_test_label[select_data])})
+        print(f"Saving 6...")
+        data_assemble.to_netcdf(f'/{self.directory}/{self.climate}_{self.variable_translate(variable).lower()}_{self.mask_str}_{self.method}_test6.nc')
+        
+        data_assemble=data_assemble.close()
+        data=data.close()
+        return
+    
+    
+    def month_method(self, data, variable):
+        
+        """Parses the test data into monthly groups, saving each individually to avoid memory issues during evaluation.
+        
+        Args:
+            data (Xarray data array): Test data for respective variable.
+            variable (str): The variable being processed and saved.
+        
+        """
+        return
+    
+    
+    def season_method(self, data, variable):
+        
+        """Parses the test data into seasonal groups, saving each individually to avoid memory issues during evaluation.
+        
+        Args:
+            data (Xarray data array): Test data for respective variable.
+            variable (str): The variable being processed and saved.
+        
+        """
+        return
+    
+    
+    def year_method(self, data, variable):
+        
+        """Parses the test data into yearly groups, saving each individually to avoid memory issues during evaluation.
+        
+        Args:
+            data (Xarray data array): Test data for respective variable.
+            variable (str): The variable being processed and saved.
+        
+        """
+        return
+    
